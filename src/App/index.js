ca16deb1ff24aaa330e412657aca5725432dfbd8
sjbgit <sjb_strat@hotmail.com>
2019-01-31 17:50:12 -0800

    added styled component

--- src/App/index.js
+++ src/App/index.js
@@ -1,12 +1,30 @@
 import React, { Component } from 'react';
 import './App.css';
 import WelcomeMessage from './WelcomeMessage';
+import styled, {css} from 'styled-components';
+
+const MyButton = styled.div `
+  color: green;
+  ${props => props.primary && css`
+    background: yelllow;
+    color: palevioletred;
+  `}
+`
+const TomatoButton = styled(MyButton)`
+  color: tomato;
+  border-color: tomato;
+`;
 
 
 class App extends Component {
   render() {
     return (
-      <WelcomeMessage name="CryptoDash"/>
+      <div>
+        <WelcomeMessage name="CryptoDash"/>
+        <MyButton>Hello</MyButton>
+        <MyButton primary>Hello</MyButton>
+        <TomatoButton>Tomato</TomatoButton>
+      </div>
     );
   }
 }
